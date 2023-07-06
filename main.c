e331abbb719f078dd3e68f7b2f3709909830887a
pacoDan <jhonpaco@frba.utn.edu.ar>
2023-07-06 00:32:06 -0300

    tratando de generalizar

--- main.c
+++ main.c
@@ -1,54 +1,71 @@
 #include <stdio.h>
+#include <string.h>
 
-char nextCharacter(char current) {
-    // Definir el alfabeto personalizado
-    char alphabet[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    int alphabetSize = sizeof(alphabet) / sizeof(alphabet[0]);
+char alphabet[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+int alphabetSize = sizeof(alphabet) / sizeof(alphabet[0]);
 
-    // Encontrar el índice del carácter actual en el alfabeto
-    int currentIndex = 0;
+int getIndex(char character) {
     for (int i = 0; i < alphabetSize; i++) {
-        if (alphabet[i] == current) {
-            currentIndex = i;
-            break;
+        if (alphabet[i] == character) {
+            return i;
         }
     }
+    return -1;  // Carácter no encontrado en el alfabeto
+}
 
-    // Calcular el índice del siguiente carácter según las reglas
-    int nextIndex;
-    if (current == 'A') {
-        nextIndex = (currentIndex + 1) % alphabetSize;  // Incrementar de A a B
-    } else if (current == 'Y') {
-        nextIndex = (currentIndex + 1) % alphabetSize;  // Incrementar de Y a Z
-    } else if (current == 'Z') {
-        nextIndex = 0;                                  // Z + 1 = 0
-    } else if (current == '9') {
-        nextIndex = (currentIndex + 1) % alphabetSize;  // Incrementar de 9 a A
-    } else {
-        // Si el carácter no cumple ninguna regla, devolvemos el mismo carácter
-        return current;
+char getNextCharacter(char character) {
+    int currentIndex = getIndex(character);
+    if (currentIndex == -1) {
+        return character;  // Carácter no encontrado, no se puede incrementar
     }
 
-    // Devolver el siguiente carácter según el índice calculado
+    int nextIndex = (currentIndex + 1) % alphabetSize;
     return alphabet[nextIndex];
 }
 
+void incrementString(char *string) {
+    int length = strlen(string);
+    int carry = 1;
+
+    for (int i = length - 1; i >= 0; i--) {
+        if (carry == 0) {
+            break;
+        }
+
+        char current = string[i];
+        char next = getNextCharacter(current);
+
+        // Actualizar el carácter actual
+        string[i] = next;
+
+        // Si el carácter actual no cambió, es un desbordamiento
+        if (next != current) {
+            carry = 0;
+        }
+    }
+
+    if (carry == 1) {
+        // Desbordamiento
+        int newSize = length + 2;
+        char newString[newSize];
+        newString[0] = alphabet[0];
+        strncpy(newString + 1, string, length + 1);
+        newString[newSize - 1] = '\0';
+        printf("Overflow occurred! New string: %s\n", newString);
+        strcpy(string, newString);
+    }
+}
+
 int main() {
-    char input = 'A';
-    char next = nextCharacter(input);
-    printf("Next character after %c: %c\n", input, next);
+    char input[] = "ASaDqEz";
+    printf("Current: %s\n", input);
+    incrementString(input);
+    printf("Next: %s\n", input);
 
-    input = 'Y';
-    next = nextCharacter(input);
-    printf("Next character after %c: %c\n", input, next);
-
-    input = 'Z';
-    next = nextCharacter(input);
-    printf("Next character after %c: %c\n", input, next);
-
-    input = '9';
-    next = nextCharacter(input);
-    printf("Next character after %c: %c\n", input, next);
+    char input2[] = "AA";
+    printf("Current: %s\n", input2);
+    incrementString(input2);
+    printf("Next: %s\n", input2);
 
     return 0;
 }
