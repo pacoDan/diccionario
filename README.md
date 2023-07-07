47db82e271e5d42f5818a200111747ce1e7555ba
pacoDan <jhonpaco@frba.utn.edu.ar>
2023-07-07 19:44:55 -0300

    probando conectar con SQL server

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# diccionario
+# para ejecutar 
+gcc main.c -o programa -lodbc
+
+
+# para usarlo se instala unixodbc https://www.unixodbc.org/ 
+# EXTRA 
+Si tienes la fuente de UnixODBC y deseas compilarla e enlazarla con tu programa C en Arch Linux, puedes seguir estos pasos:
+
+Extrae el archivo unixODBC-2.3.11.tar.gz en tu directorio de trabajo. Puedes hacerlo con el siguiente comando:
+shell
+Copy code
+tar -xf unixODBC-2.3.11.tar.gz
+Ve al directorio extraído unixODBC-2.3.11:
+shell
+Copy code
+cd unixODBC-2.3.11
+Configura la compilación ejecutando el comando ./configure. Esto preparará el entorno y configurará las opciones de compilación:
+shell
+Copy code
+./configure
+Compila las fuentes ejecutando el comando make. Esto compilará UnixODBC:
+shell
+Copy code
+make
+Instala UnixODBC ejecutando el comando sudo make install. Esto instalará las bibliotecas y archivos necesarios en tu sistema:
+shell
+Copy code
+sudo make install
+Una vez instalado UnixODBC, podrás enlazarlo con tu programa C. Asegúrate de incluir las bibliotecas necesarias y enlazar con UnixODBC al compilar tu programa. Puedes usar el siguiente comando de compilación como ejemplo:
+
+shell
+Copy code
+gcc main.c -o programa -lodbc
+Reemplaza main.c con el nombre de tu archivo fuente C y programa con el nombre que desees para el ejecutable resultante.
+
+Recuerda que es posible que también necesites configurar los controladores ODBC específicos para SQL Server. En ese caso, consulta la documentación proporcionada junto con los controladores y sigue las instrucciones para configurar los controladores en UnixODBC.
+
+Es importante tener en cuenta que compilar e instalar software desde la fuente puede requerir dependencias adicionales y puede no ser compatible con las herramientas de gestión de paquetes de la distribución. Siempre es recomendable consultar la documentación proporcionada con la fuente y considerar otras opciones disponibles, como la búsqueda de paquetes precompilados o el uso de alternativas compatibles con tu distribución Linux.
+
+
+
+
--- main.c
+++ main.c
@@ -1,55 +1,130 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include <sql.h>
+#include <sqlext.h>
 
-char alphabet[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-int alphabetSize = sizeof(alphabet) / sizeof(alphabet[0]);
+void printODBCError(SQLHANDLE handle, SQLSMALLINT handleType, SQLRETURN ret)
+{
+    SQLCHAR SQLState[6], message[SQL_MAX_MESSAGE_LENGTH];
+    SQLINTEGER nativeError;
+    SQLSMALLINT i, messageLength;
 
-int getIndex(char character) {
-    for (int i = 0; i < alphabetSize; i++) {
-        if (alphabet[i] == character) {
-            return i;
-        }
-    }
-    return -1;  // Carácter no encontrado en el alfabeto
-}
-
-char getNextCharacter(char character) {
-    int currentIndex = getIndex(character);
-    if (currentIndex == -1) {
-        return character;  // Carácter no encontrado, no se puede incrementar
-    }
-
-    int nextIndex = (currentIndex + 1) % alphabetSize;
-    return alphabet[nextIndex];
-}
-
-void incrementString(char *string) {
-    int length = strlen(string);
-    int carry = 1;
-
-    for (int i = length - 1; i >= 0; i--) {
-        if (carry == 0) {
-            break;
-        }
-
-        char current = string[i];
-        char next = getNextCharacter(current);
-
-        // Actualizar el carácter actual
-        string[i] = next;
-
-        // Si el carácter actual no cambió, es un desbordamiento
-        if (next != current) {
-            carry = 0;
-        }
+    if (SQL_SUCCESS == SQLGetDiagRec(handleType, handle, 1, SQLState, &nativeError, message, sizeof(message), &messageLength))
+    {
+        printf("ODBC Error: %s - %s\n", SQLState, message);
     }
 }
 
-int main() {
-    char input[] = "ASaDqEz";
-    printf("Current: %s\n", input);
-    incrementString(input);
-    printf("Next: %s\n", input);
+int main()
+{
+    SQLHENV env;
+    SQLHDBC dbc;
+    SQLRETURN ret;
+
+    // Inicializar el entorno
+    ret = SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &env);
+    if (ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO)
+    {
+        printf("Error al inicializar el entorno\n");
+        return 1;
+    }
+
+    ret = SQLSetEnvAttr(env, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, SQL_IS_INTEGER);
+    if (ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO)
+    {
+        printf("Error al configurar la versión de ODBC\n");
+        SQLFreeHandle(SQL_HANDLE_ENV, env);
+        return 1;
+    }
+
+    // Inicializar la conexión
+    ret = SQLAllocHandle(SQL_HANDLE_DBC, env, &dbc);
+    if (ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO)
+    {
+        printf("Error al inicializar la conexión\n");
+        SQLFreeHandle(SQL_HANDLE_ENV, env);
+        return 1;
+    }
+
+    ret = SQLDriverConnect(dbc, NULL, (SQLCHAR *)"DRIVER={SQL Server};SERVER=568e70c42d9f;DATABASE=diccionario;UID=sa;PWD=Pi3141592654!;", SQL_NTS, NULL, 0, NULL, SQL_DRIVER_COMPLETE);
+    if (ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO)
+    {
+        printf("Error al conectar a la base de datos\n");
+        printODBCError(dbc, SQL_HANDLE_DBC, ret);
+        SQLFreeHandle(SQL_HANDLE_DBC, dbc);
+        SQLFreeHandle(SQL_HANDLE_ENV, env);
+        return 1;
+    }
+
+    int numero = 123;
+    char texto[] = "ejemplo_texto";
+
+    SQLCHAR *insertQuery = (SQLCHAR *)"INSERT INTO Ejemplo (Numero, Texto) VALUES (?, ?);";
+    SQLINTEGER numeroValue = numero;
+    SQLLEN numeroValueLen = sizeof(numeroValue);
+    SQLCHAR *textoValue = texto;
+    SQLLEN textoValueLen = SQL_NTS;
+
+    SQLHSTMT stmt;
+    ret = SQLAllocHandle(SQL_HANDLE_STMT, dbc, &stmt);
+    if (ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO)
+    {
+        printf("Error al inicializar la declaración\n");
+        printODBCError(dbc, SQL_HANDLE_DBC, ret);
+        SQLFreeHandle(SQL_HANDLE_DBC, dbc);
+        SQLFreeHandle(SQL_HANDLE_ENV, env);
+        return 1;
+    }
+
+    ret = SQLPrepare(stmt, insertQuery, SQL_NTS);
+    if (ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO)
+    {
+        printf("Error al preparar la declaración\n");
+        printODBCError(stmt, SQL_HANDLE_STMT, ret);
+        SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+        SQLFreeHandle(SQL_HANDLE_DBC, dbc);
+        SQLFreeHandle(SQL_HANDLE_ENV, env);
+        return 1;
+    }
+
+    ret = SQLBindParameter(stmt, 1, SQL_PARAM_INPUT, SQL_C_SLONG, SQL_INTEGER, 0, 0, &numeroValue, 0, &numeroValueLen);
+    if (ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO)
+    {
+        printf("Error al enlazar el parámetro 1\n");
+        printODBCError(stmt, SQL_HANDLE_STMT, ret);
+        SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+        SQLFreeHandle(SQL_HANDLE_DBC, dbc);
+        SQLFreeHandle(SQL_HANDLE_ENV, env);
+        return 1;
+    }
+
+    ret = SQLBindParameter(stmt, 2, SQL_PARAM_INPUT, SQL_C_CHAR, SQL_VARCHAR, sizeof(texto), 0, textoValue, sizeof(texto), &textoValueLen);
+    if (ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO)
+    {
+        printf("Error al enlazar el parámetro 2\n");
+        printODBCError(stmt, SQL_HANDLE_STMT, ret);
+        SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+        SQLFreeHandle(SQL_HANDLE_DBC, dbc);
+        SQLFreeHandle(SQL_HANDLE_ENV, env);
+        return 1;
+    }
+
+    ret = SQLExecute(stmt);
+    if (ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO)
+    {
+        printf("Error al ejecutar la declaración\n");
+        printODBCError(stmt, SQL_HANDLE_STMT, ret);
+        SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+        SQLFreeHandle(SQL_HANDLE_DBC, dbc);
+        SQLFreeHandle(SQL_HANDLE_ENV, env);
+        return 1;
+    }
+
+    printf("La inserción se realizó correctamente\n");
+
+    SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+    SQLFreeHandle(SQL_HANDLE_DBC, dbc);
+    SQLFreeHandle(SQL_HANDLE_ENV, env);
 
     return 0;
 }
